104f0dd2008dbaa8fb3cbc643074fc45ea769cea
jure1989 <jure.uran@gmail.com>
2024-02-12 16:37:53 +0100

    init

---
+++ src/modules/app.module.ts
@@ -0,0 +1,23 @@
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { configValidationSchema } from 'config/schema.config'
+import { LoggerMiddleware } from 'middleware/logger.middleware'
+import { DatabaseModule } from './database/database.module'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
+    DatabaseModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({ path: '*', method: RequestMethod.ALL })
+  }
+}
